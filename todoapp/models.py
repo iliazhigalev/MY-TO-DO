edbba2ba6f505f6c17ae2cb6c74d72166276ac96
ILIA <iliazhigalev@yandex.ru>
2023-11-09 18:38:50 +0300

    add models.py

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class ToDo(models.Model):
+    ''' Класс, описывающий модель приложения в бд '''
+
+    title = models.CharField(max_length=150, verbose_name='Название задания')
+    is_complete = models.BooleanField('Завершено', default=False)
+    time = models.DateTimeField(auto_now_add=True)  # дата, создания задания
+
+    class Meta:
+        verbose_name = 'Задание'
+        verbose_name_plural = 'Задания'
+
+    def __str__(self):
+        return self.title
